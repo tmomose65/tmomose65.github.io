1aaeaf2c3fd1e908e482804709d011ab43ba21b5
tmomose <t.momose65@gmail.com>
2016-08-14 07:29:51 +0000

    kansei

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+  $('a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop': position
+    },500);
+  });
+  
+});
